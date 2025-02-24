c0ed90238b9846a6dbe2c8433478508a2fffdb16
Ian Wong <iwong12@calpoly.edu>
2025-02-24 09:54:17 -0800

    Created Mongo Schemas

--- packages/express-backend/models/user.js
+++ packages/express-backend/models/user.js
@@ -7,19 +7,94 @@ const UserSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
     job: {
       type: String,
       required: true,
-      trim: true,
-      validate(value) {
-        if (value.length < 2)
-          throw new Error("Invalid job, must be at least 2 characters.");
-      },
+    },
+    location: {
+      type: String,
+    },
+    date_joined: {
+      type: Date,
+      default: Date.now(),
     },
   },
   { collection: "users_list" },
 );
 
-const User = mongoose.model("User", UserSchema);
+const ListingSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+    },
+    images: {
+      type: [{ type: String }],
+    },
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    expiresAt: {
+      type: Date,
+      default: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+    },
+  },
+  { collection: "listings" },
+);
 
-export default User;
+const MessageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    listing: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Listing",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  { collection: "messages" },
+);
+
+const User = mongoose.model("User", UserSchema);
+const Listing = mongoose.model("Listing", ListingSchema);
+const Message = mongoose.model("Message", MessageSchema);
+
+export { User, Listing, Message };
