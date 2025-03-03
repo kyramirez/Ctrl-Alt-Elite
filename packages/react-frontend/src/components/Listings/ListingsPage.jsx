0d4637b7e4de7497c858583e84c8c721d52d6540
Ian Wong <iwong12@calpoly.edu>
2025-03-02 23:28:41 -0800

    Images and UI

--- packages/react-frontend/src/components/Listings/ListingsPage.jsx
+++ packages/react-frontend/src/components/Listings/ListingsPage.jsx
@@ -1,28 +1,38 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./ListingsPage.css";
+import logo from "../../assets/logo.jfif";
 
 const sampleListings = [
   {
     title: "Smartphone",
-    imageUrl: "https://source.unsplash.com/300x200/?smartphone",
+    imageUrl:
+      "https://images.unsplash.com/photo-1634403665481-74948d815f03?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
-  { title: "Laptop", imageUrl: "https://source.unsplash.com/300x200/?laptop" },
+  {
+    title: "Laptop",
+    imageUrl:
+      "https://images.unsplash.com/photo-1531297484001-80022131f5a1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bGFwdG9wfGVufDB8fDB8fHwy",
+  },
   {
     title: "Gaming Console",
-    imageUrl: "https://source.unsplash.com/300x200/?gaming",
+    imageUrl:
+      "https://images.unsplash.com/photo-1588689115724-a624efec3c93?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2FtZSUyMGNvbnNvbGV8ZW58MHx8MHx8fDI%3D",
   },
   {
     title: "Bicycle",
-    imageUrl: "https://source.unsplash.com/300x200/?bicycle",
+    imageUrl:
+      "https://images.unsplash.com/photo-1485965120184-e220f721d03e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmlrZXxlbnwwfHwwfHx8Mg%3D%3D",
   },
   {
     title: "Sneakers",
-    imageUrl: "https://source.unsplash.com/300x200/?sneakers",
+    imageUrl:
+      "https://images.unsplash.com/photo-1605348532760-6753d2c43329?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c25lYWtlcnxlbnwwfHwwfHx8Mg%3D%3D",
   },
   {
     title: "Furniture",
-    imageUrl: "https://source.unsplash.com/300x200/?furniture",
+    imageUrl:
+      "https://images.unsplash.com/photo-1618220179428-22790b461013?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZnVybml0dXJlfGVufDB8fDB8fHwy",
   },
 ];
 
@@ -32,9 +42,14 @@ function ListingsPage() {
   return (
     <div className="container">
       <header className="header">
-        <img src="/assets/logo.png" alt="Logo" className="logo" />
+        <button className="logo-button" onClick={() => navigate("/")}>
+          <img src={logo} alt="Logo" className="logo" />
+        </button>
         <button className="profile-button" onClick={() => navigate("/account")}>
-          <img src="/assets/profile-icon.png" alt="Profile" />
+          <img
+            src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cHJvZmlsZSUyMGljb258ZW58MHx8MHx8fDI%3D"
+            alt="Profile"
+          />
         </button>
       </header>
 
