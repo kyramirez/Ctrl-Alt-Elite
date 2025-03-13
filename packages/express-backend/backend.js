2f9cb5bdba5d25dac470f46606f7c13f4c319321
kyramirez <kramir62@calpoly.edu>
2025-03-12 18:38:25 -0700

    ugh

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -18,7 +18,7 @@ mongoose
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = 8080;
+const port = 8000;
 
 app.post("/signup", registerUser);
 app.post("/login", loginUser);
