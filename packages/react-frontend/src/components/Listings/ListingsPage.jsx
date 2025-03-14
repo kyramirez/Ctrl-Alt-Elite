b07312d76da4f8a3088e250940228d290d763341
kyramirez <kramir62@calpoly.edu>
2025-03-14 10:54:35 -0700

    maybe?

--- packages/react-frontend/src/components/Listings/ListingsPage.jsx
+++ packages/react-frontend/src/components/Listings/ListingsPage.jsx
@@ -10,7 +10,7 @@ function ListingsPage({ addAuthHeader, resetToken }) {
 
   useEffect(() => {
     fetch(
-      "http://freebiefinders-h3dtdeacb5gtc8b0.westus3-01.azurewebsites.net",
+      "http://freebiefinders-h3dtdeacb5gtc8b0.westus3-01.azurewebsites.net/listings",
       {
         headers: addAuthHeader(),
       },
