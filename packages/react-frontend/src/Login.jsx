5973d11e4b55268f606ce3e6680583e13d937e42
Ian Wong <iwong12@calpoly.edu>
2025-03-17 14:31:29 -0700

    Fixing test suite

--- packages/react-frontend/src/Login.jsx
+++ packages/react-frontend/src/Login.jsx
@@ -100,7 +100,7 @@ function Login(props) {
         <input
           type="password"
           name="pwd"
-          id="pwd"
+          id="password"
           value={creds.pwd}
           onChange={handleChange}
           style={inputStyle}
